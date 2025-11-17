1258f15249507b2f1a6d8f6cc5d5a527a704c553
MUBASHIR <mubashirpp695@gmail.com>
2025-11-17 10:54:28 +0530

    .env added

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCvMhNwNXC7XGLBLcJZ6bj7TzVGsk0CXpQ",
-    authDomain: "blocq-the-blogapp.firebaseapp.com",
-    projectId: "blocq-the-blogapp",
-    storageBucket: "blocq-the-blogapp.firebasestorage.app",
-    messagingSenderId: "779508495928",
-    appId: "1:779508495928:web:ccfc9e4ee8276c127618cd",
-    measurementId: "G-C51HQSCT2F"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
